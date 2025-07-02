30581291373946dc5d7a65ee058d64b391876ee9
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2025-07-03 00:45:42 +0530

    Added Controller for User API

--- src/main/java/org/example/ecommercespring/controllers/UserController.java
+++ src/main/java/org/example/ecommercespring/controllers/UserController.java
@@ -1,4 +1,18 @@
 package org.example.ecommercespring.controllers;
 
+import org.example.ecommercespring.services.IUserService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final IUserService userService;
+
+    public UserController(IUserService _userService) {
+        this.userService = _userService;
+    }
+
+    
 }
